9ecdb4d917292fe6330f6427022f96a155e91b0a
olzhastyle <o.abdrakhman@gmail.com>
2021-04-05 01:19:50 +0600

    about

--- pages/components/Homepage.vue
+++ pages/components/Homepage.vue
@@ -8,7 +8,7 @@
                 <a href="/about">Start Learn!</a>
             </div>
             <div class="button-about">
-                <a href="/about">About пасхалка1</a>
+                <a href="/about">About пасхалка</a>
             </div>
            </div>
         </div>
