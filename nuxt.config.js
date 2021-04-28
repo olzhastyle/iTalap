7aa4daeb82c4f2ba580a2cdb5a038b7e7bab9ed6
olzhas <o.abdrakhman@gmail.com>
2021-04-28 13:02:41 +0600

    master

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: './plugins/vue-slick-carousel.js' }
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
